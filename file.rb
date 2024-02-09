06109ad35345397babe821b0014f74c0b6556867
Habib <a.habibsultani@gmail.com>
2024-02-09 18:26:36 +0300

    Update file.rb

--- file.rb
+++ file.rb
@@ -48,3 +48,51 @@ end
 p check_num(9, 12)
 p check_num(11, 11)
 p check_num(3, 13)
+
+# frozen_string_literal: true
+
+# BUU' na istisna kapsaminde uygun olan kelimeleri kontrol eden metot
+def is_an_exception?(word_to_search) 
+  
+  # BUU' na istisna kapsaminde uygun olan kelimeler listesidir. Degistirmeyiniz!
+  exception_words = %w[anne dahi elma hangi hani inanmak] 
+
+  return exception_words.include?(word_to_search)
+    
+end
+
+# BUU aranmayan kelimeleri kontrol eden metot
+def is_not_required?(word_to_search)
+
+  # BUU aranmayan kelimeler listesidir. Degistirmeyiniz!
+  not_required_words = %w[ahenk badem ceylan fidan gazete hamsi kestane limon model otomatik pehlivan selam tiyatro viraj ziyaret]
+  
+  return not_required_words.include?(word_to_search)
+    
+end
+
+# Bir kelimenin BUU' na uygun olup/olmadigini bildiren metot
+def is_harmonic?(word_to_search) 
+
+  #Kontrol icin kullanilacak kalin ve ince harfler
+  thick_vowels = word_to_search.match(/[aıou]/i)
+  thin_vowels = word_to_search.match(/[eiöü]/i)
+  
+  thick_vowels.nil? ^ thin_vowels.nil?
+
+end
+
+
+words = gets.chomp.split # Kelimeleri al
+
+for word in words
+  if is_an_exception?(word)
+    puts "#{word} : ISTSN"
+   elsif is_not_required?(word)
+    puts "#{word} : ARANMZ"
+   elsif is_harmonic?(word)
+    puts "#{word} : UYGUN"
+   else 
+    puts "#{word} : UYGNDGL"
+  end 
+end
